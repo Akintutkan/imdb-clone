6b8540c098d2f96c82c2f1692c08c60ece4bf4d1
Akin94 <akin.tutkan@gmail.com>
2023-03-22 21:42:27 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import Header from "./components/Header";
+import Favorites from "components/Favorites";
+import Movies from "components/Movies";
 import './App.css';
+import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+     <Router>
+      <div className="container mx-auto grid gap-y-6 pt-8"> 
+       <Header/>
+        <Routes>
+          <Route exact path="/" element={<Movies/>} /> 
+          <Route path="/favorites" element={<Favorites/>} /> 
+        </Routes>
+      </div>
+    </Router>
+      
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {MdFavorite} from "react-icons/md" 
+import { Link } from 'react-router-dom'
+const Header = () => {
+  return (
+    <div className='bg-black'>
+      <div className='h-14 sm:h-11 container mx-auto  text-white flex items-center justify-center sm:justify-between'>
+        <Link to="/">Fakeİmdb</Link>
+        <nav className='gap-x-8 hidden sm:flex font-semibold'>
+          <Link to="/movies" className='flex items-center gap-x-2 text-white transition-all text-opacity-80 hover:text-opacity-100'>Movies </Link>
+          <Link to="/favorites" className='flex items-center gap-x-2 text-white transition-all text-opacity-80 hover:text-opacity-100'>Favorites
+          <MdFavorite size={20}/>
+          </Link>
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+export default Header
